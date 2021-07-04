3fe5c1896a320e12864f5c64bee0bb7b168da875
Vishwanath Pethri Kamath <vishwanathkamath@gmail.com>
2021-07-04 06:21:40 +0000

    Updates

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -95,7 +95,7 @@ app.get("/text/sentiment", (req,res) => {
     });
 });
 
-let server = app.listen(8081, () => {
+let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
